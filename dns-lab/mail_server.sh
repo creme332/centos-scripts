8441f4acd3f6d62619e78b162f65584d44358fa4
creme332 <65414576+creme332@users.noreply.github.com>
2025-08-11 21:06:20 +0400

    if yum is not setup properly, run script for setting it up

--- dns-lab/mail_server.sh
+++ dns-lab/mail_server.sh
@@ -42,8 +42,20 @@ fi
 if yum repolist enabled >/dev/null 2>&1 && yum makecache fast >/dev/null 2>&1; then
     echo "YUM OK"
 else
-    echo "YUM NOT WORKING. Run yum.sh" >&2
-    exit 1
+    echo "Setting up YUM..." >&2
+    curl -fsSL https://raw.githubusercontent.com/creme332/centos-scripts/refs/heads/main/yum.sh -o /tmp/yum.sh && \
+    bash /tmp/yum.sh || {
+        echo "Failed to set up YUM. Exiting."
+        exit 1
+    }
+
+    # Re-check YUM after setup
+    if yum repolist enabled >/dev/null 2>&1 && yum makecache fast >/dev/null 2>&1; then
+        echo "YUM fixed and working."
+    else
+        echo "YUM setup failed. Exiting."
+        exit 1
+    fi
 fi
 
 # Function to check if a package is installed
