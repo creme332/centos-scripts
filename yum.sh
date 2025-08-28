97da69e25fde23972e20824cb34b79255d51bfaf
creme332 <65414576+creme332@users.noreply.github.com>
2025-08-28 12:44:35 +0400

    fix CentOS-SCLo-scl.repo and CentOS-SCLo-scl-rh.repo, improve verification
    - hardcode architecture since only supported one is x86
    - remove unnecessary comments
    - check number of repos discovered by yum after modification

--- yum.sh
+++ yum.sh
@@ -5,10 +5,11 @@
 # Description: Updates repository urls so that Yum can keep functioning
 #              despite CentOS being discontinued. You can run the script
 #              using bash yum.sh
-# Version: 0.4
+# Version: 0.5
 # Author: creme332
 #--------------------------------------------------------------
 # Requirements:
+# - x86_64 architecture
 # - CentOS:7.9.2009 with sudo privileges
 # - Internet connectivity for package installation
 #--------------------------------------------------------------
@@ -30,38 +31,81 @@ else
     exit 1
 fi
 
-# Backup the original CentOS-Base.repo file before modifying
+# Backup configuration files
 cp /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup.$(date +%F-%T)
+cp /etc/yum.repos.d/CentOS-SCLo-scl.repo /etc/yum.repos.d/CentOS-SCLo-scl.repo.backup.$(date +%F-%T)
+cp /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo.backup.$(date +%F-%T)
 
 # Write the new content to CentOS-Base.repo
 cat <<EOL > /etc/yum.repos.d/CentOS-Base.repo
 [base]
 name=CentOS-\$releasever - Base
-baseurl=https://vault.centos.org/7.9.2009/os/\$basearch
+baseurl=https://vault.centos.org/7.9.2009/os/x86_64
 gpgcheck=1
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
 
-#released updates
 [updates]
 name=CentOS-\$releasever - Updates
-baseurl=https://vault.centos.org/7.9.2009/updates/\$basearch
+baseurl=https://vault.centos.org/7.9.2009/updates/x86_64
 gpgcheck=1
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
 
-#additional packages that may be useful
 [extras]
 name=CentOS-\$releasever - Extras
-baseurl=https://vault.centos.org/7.9.2009/extras/\$basearch
+baseurl=https://vault.centos.org/7.9.2009/extras/x86_64
 gpgcheck=1
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
 
-#additional packages that extend functionality of existing packages
 [centosplus]
 name=CentOS-\$releasever - Plus
-baseurl=https://vault.centos.org/7.9.2009/centosplus/\$basearch
+baseurl=https://vault.centos.org/7.9.2009/centosplus/x86_64
 gpgcheck=1
 enabled=0
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+EOL
+
+# Fix CentOS-SCLo-sclo.repo
+cat <<EOL > /etc/yum.repos.d/CentOS-SCLo-scl.repo
+[centos-sclo-sclo]
+name=CentOS-7 - SCLo sclo
+baseurl=https://vault.centos.org/7.9.2009/sclo/x86_64/sclo/
+gpgcheck=1
+enabled=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-SIG-SCLo
+
+[centos-sclo-sclo-testing]
+name=CentOS-7 - SCLo sclo Testing
+enabled=0
+
+[centos-sclo-sclo-source]
+name=CentOS-7 - SCLo sclo Sources
+enabled=0
+
+[centos-sclo-sclo-debuginfo]
+name=CentOS-7 - SCLo sclo Debuginfo
+enabled=0
+EOL
+
+# Fix CentOS-SCLo-scl-rh.repo
+cat <<EOL > /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo
+[centos-sclo-rh]
+name=CentOS-7 - SCLo rh
+baseurl=https://vault.centos.org/7.9.2009/sclo/x86_64/rh/
+gpgcheck=1
+enabled=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-SIG-SCLo
+
+[centos-sclo-rh-testing]
+name=CentOS-7 - SCLo rh Testing
+enabled=0
+
+[centos-sclo-rh-source]
+name=CentOS-7 - SCLo rh Sources
+enabled=0
+
+[centos-sclo-rh-debuginfo]
+name=CentOS-7 - SCLo rh Debuginfo
+enabled=0
 EOL
 
 # Kill any process locking yum
@@ -105,5 +149,12 @@ if ! grep -q "$GOOGLE_DNS2" $RESOLV_CONF; then
     echo "$GOOGLE_DNS2" >> $RESOLV_CONF
 fi
 
-# Inform the user of the change
-echo "Setup complete."
+# Verify that Yum sees enabled repositories
+REPO_COUNT=$(yum repolist enabled | awk '/repolist:/{print $2}' | tr -d ',')
+
+if [[ -z "$REPO_COUNT" || "$REPO_COUNT" -eq 0 ]]; then
+    echo "Error: No enabled Yum repositories found. Yum will not work!"
+    exit 1
+else
+    echo "Yum is configured correctly. Enabled repositories: $REPO_COUNT"
+fi
